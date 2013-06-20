7c36d3722bb35e3a4e1293847179d31df58a7a3e
Eirik Refsdal <eirikref@gmail.com>
2013-06-20 22:41:10 +0200

    Initial commit
    Initil commit

---
+++ src/README.md
@@ -0,0 +1,17 @@
+# Loom
+
+Loom is what one day might become my simple, small, personal go-to PHP library/framework. For now it is nothing but work-in-progress, and not meant to be used by anyone.
+
+## Background
+
+I used to work for one of the dinosaur Internet companies for many years, and spent many nights and weekends writing the PHP framework upon which I created internal tools and websites.
+
+When I left the company, my PHP library/framework stayed behind. I could of course start using Zend Framework 2, Phalcon, or similar, but I guess half the fun is in writing the library itself.
+
+So who knows if Loom will ever be anything more than a dream of what I once implemented and wish I could recreate from memory.
+
+## Status
+
+Currently I'm just touching some files, outlining classes, and not really implementing much. I will outline more classes, write tests, and implement code as I feel like it, but I have no schedules or plans.
+
+It will be done when it is done.
---
+++ src/Settable.php
@@ -0,0 +1,122 @@
+<?php
+/**
+ * Loom: Settable
+ * Copyright (c) 2013 Eirik Refsdal <eirikref@gmail.com>
+ */
+
+namespace Loom;
+
+/**
+ * Loom: Settable
+ *
+ * Class providing a simple interface for setting and getting
+ * variables with type-checking.
+ *
+ * @package Loom
+ * @version 2013-06-20
+ * @author  Eirik Refsdal <eirikref@gmail.com>
+ */
+class Settable
+{
+
+    /**
+     * Array containing data
+     *
+     * @var    array $data
+     * @access private
+     */
+    private $data = array();
+
+    /**
+     * Delimiter for supporting multi-level keys, ie. "config.db.mysql.host"
+     *
+     * @var    string $delimiter
+     * @access private
+     */
+    private $delimiter = ".";
+
+
+
+    /**
+     * Set a value
+     *
+     * @author Eirik Refsdal <eirikref@gmail.com>
+     * @since  2013-06-20
+     * @access public
+     * @return object $this
+     *
+     * @param  string $key   The variable name
+     * @param  mixed  $value The value
+     * @param  string $type  Set for strict type checking
+     */
+    public function set($key, $value, $type = null)
+    {
+    }
+
+
+
+    /**
+     * Get a value
+     *
+     * @author Eirik Refsdal <eirikref@gmail.com>
+     * @since  2013-06-20
+     * @access public
+     * @return mixed 
+     *
+     * @param  string $key  The variable name
+     * @param  string $type Set for strict type checking
+     */
+    public function get($key, $type = null)
+    {
+    }
+
+
+
+    /**
+     * Remove a value
+     *
+     * @author Eirik Refsdal <eirikref@gmail.com>
+     * @since  2013-06-20
+     * @access public
+     * @return object $this
+     *
+     * @param  string $key The variable name
+     */
+    public function remove($key)
+    {
+    }
+
+
+
+    /**
+     * Set delimiter for multi-level keys
+     *
+     * @author Eirik Refsdal <eirikref@gmail.com>
+     * @since  2013-06-20
+     * @access public
+     * @return object $this
+     *
+     * @param  string $delimiter The delimiter
+     */
+    public function setDelimiter($delimiter)
+    {
+    }
+
+
+
+    /**
+     * Checks if the given value matches the given type, ie. if an
+     * integer actually is an integer.
+     *
+     * @author Eirik Refsdal <eirikref@gmail.com>
+     * @since  2013-06-20
+     * @access private
+     * @return boolean
+     *
+     * @param  mixed  $value The value
+     * @param  string $type  The required type
+     */
+    private function checkType($item, $type)
+    {
+    }
+}
