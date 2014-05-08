5c6cae8813cda79c5aa884ecb0e6f2a890c3a1b9
Eirik Refsdal <eirikref@gmail.com>
2014-05-08 17:06:41 +0200

    Test in a more orderly fashion

---
+++ tests/unit/Settable/SetInvalidKeyTest.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * Loom: Unit tests
+ * Copyright (c) 2014 Eirik Refsdal <eirikref@gmail.com>
+ */
+
+namespace Loom\Tests\Unit\Settable;
+
+/**
+ * Loom: Unit tests for making sure Settable::set() behaves correctly
+ * when given invalid keys.
+ *
+ * @package    Fiber
+ * @subpackage Tests
+ * @version    2014-05-08
+ * @author     Eirik Refsdal <eirikref@gmail.com>
+ */
+class SetInvalidKeyTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Data provider with non-strings, to be used for testing invalid
+     * keys
+     *
+     * @author Eirik Refsdal <eirikref@gmail.com>
+     * @since  2014-05-08
+     * @access public
+     * @return array
+     */
+    public function getNonStrings()
+    {
+        return array(
+            array(11),
+            array(-3),
+            array(3.14),
+            array(true),
+            array(false),
+            array(null),
+            array(array()),
+            array(new \stdClass())
+        );
+    }
+
+
+
+    /**
+     * Test that set() handles non-string keys
+     *
+     * @test
+     * @covers       \Loom\Settable::set
+     * @dataProvider getNonStrings
+     * @author       Eirik Refsdal <eirikref@gmail.com>
+     * @since        2014-05-08
+     * @access       public
+     * @return       void
+     *
+     * @param        string $key The key
+     */
+    public function testNonStrings($key)
+    {
+        $store = new \Loom\Settable();
+        $this->assertNull($store->set($key, "myvalue"));
+        $this->assertEquals([], $store->get());
+    }
+
+
+
+    /**
+     * Test that empty string keys are not allowed
+     *
+     * @test
+     * @covers \Loom\Settable::set
+     * @author Eirik Refsdal <eirikref@gmail.com>
+     * @since  2014-05-08
+     * @access public
+     * @return void
+     */
+    public function testEmptyString()
+    {
+        $store = new \Loom\Settable();
+        $this->assertNull($store->set("", "myvalue"));
+        $this->assertEquals([], $store->get());
+    }
+
+
+
+    /**
+     * Test that too long string keys are not allowed
+     *
+     * @test
+     * @covers \Loom\Settable::set
+     * @author Eirik Refsdal <eirikref@gmail.com>
+     * @since  2014-05-08
+     * @access public
+     * @return void
+     */
+    public function testTooLongString()
+    {
+        $maxKeyLength = 128;
+        $store        = new \Loom\Settable();
+
+        $this->assertNull($store->set(str_repeat("a", $maxKeyLength + 1), "myvalue"));
+        $this->assertEquals([], $store->get());
+    }
+}
