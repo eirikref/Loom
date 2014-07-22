a074dc87aa2f36df821ff6eeed80c5ca21b9ad7e
Eirik Refsdal <eirikref@gmail.com>
2014-07-22 14:38:53 +0200

    Test empty and valid YAML files

--- tests/unit/Router/FromYamlTest.php
+++ tests/unit/Router/FromYamlTest.php
@@ -29,10 +29,52 @@ class FromYamlTest extends \PHPUnit_Framework_TestCase
      */
     public function testFromYamlWithNonExistentFile()
     {
-        $path   = "./files/nosuchfile.yaml";
+        $path   = "./tests/unit/Router/files/nosuchfile.yaml";
         $file   = \Loom\File::fromPath($path);
         $router = \Loom\Router::fromYaml($file);
 
         $this->assertNull($router);
+    }
+
+
+
+    /**
+     * Test Router::fromYaml() with an empty file
+     *
+     * @test
+     * @covers \Loom\Router::fromYaml
+     * @author Eirik Refsdal <eirikref@gmail.com>
+     * @since  2014-07-22
+     * @access public
+     * @return void
+     */
+    public function testFromYamlWithEmptyFile()
+    {
+        $path   = "./tests/unit/Router/files/empty.yaml";
+        $file   = \Loom\File::fromPath($path);
+        $router = \Loom\Router::fromYaml($file);
+
+        $this->assertNull($router);
+    }
+
+
+
+    /**
+     * Test Router::fromYaml() with a valid file
+     *
+     * @test
+     * @covers \Loom\Router::fromYaml
+     * @author Eirik Refsdal <eirikref@gmail.com>
+     * @since  2014-07-22
+     * @access public
+     * @return void
+     */
+    public function testFromYamlWithValidFile()
+    {
+        $path   = "./tests/unit/Router/files/valid.yaml";
+        $file   = \Loom\File::fromPath($path);
+        $router = \Loom\Router::fromYaml($file);
+
+        $this->assertTrue($router instanceof \Loom\Router);
     }
 }
