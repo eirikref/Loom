4265d8c02380d1b77110365505f7f68c8a01accd
Eirik Refsdal <eirikref@gmail.com>
2014-10-09 16:21:15 +0200

    Basic layout of the class
    This ought to cover everything I need.

---
+++ src/Loom/HttpRequestManager.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Loom: HttpRequestManager
+ * Copyright (c) 2014 Eirik Refsdal <eirikref@gmail.com>
+ */
+
+namespace Loom;
+
+/**
+ * Loom: HttpRequestManager
+ *
+ * @package Loom
+ * @version 2014-10-09
+ * @author  Eirik Refsdal <eirikref@gmail.com>
+ */
+class HttpRequestManager
+{
+
+    private $ua;
+    private $requests = array();
+    private $handles = array();
+
+    public function add(&$request)
+    {
+    }
+
+    public function run()
+    {
+    }
+    
+    public function empty()
+    {
+    }
+}
