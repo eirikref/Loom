9420653260637d6805f640b54a8020b796f0654c
Eirik Refsdal <eirikref@gmail.com>
2014-12-18 15:13:33 +0100

    Some implementation of the request manager

--- src/Loom/HttpRequestManager.php
+++ src/Loom/HttpRequestManager.php
@@ -15,17 +15,51 @@ namespace Loom;
  */
 class HttpRequestManager
 {
-
-    private $ua;
     private $requests = array();
     private $handles = array();
 
     public function add(&$request)
     {
+        if (!$request instanceof \Loom\Request) {
+            return;
+        }
+
+        $res = curl_init();
+
+        curl_setopt($res, CURLOPT_URL, $request->getUrl());
+        
+        $this->requests[] = $request;
+        $this->handles[]  = $res;
     }
 
     public function run()
     {
+        if (count($this->requests) < 1) {
+            return;
+        }
+
+        $mh = curl_multi_init();
+        foreach ($this->requests as $id => $r) {
+            curl_multi_add_handle($mh, $this->handles[$id]);
+        }
+
+        $active = null;
+        do {
+            $mrc = curl_multi_exec($mh, $active);
+        } while ($mrc == CURLM_CALL_MULTI_PERFORM);
+        
+        while ($active && $mrc == CURLM_OK) {
+            if (curl_multi_select($mh) != -1) {
+                do {
+                    $mrc = curl_multi_exec($mh, $active);
+                } while ($mrc == CURLM_CALL_MULTI_PERFORM);
+            }
+        }
+
+        foreach ($this->requests as $id => $r) {
+            curl_multi_remove_handle($mh, $this->handles[$id]);
+        }
+        curl_multi_close($mh);
     }
     
     public function reset()
