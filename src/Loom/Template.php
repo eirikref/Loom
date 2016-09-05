00efc5fbd7a37b2fc530809f063a9eddd21326dd
Eirik Refsdal <eirikref@gmail.com>
2016-09-05 10:26:17 +0200

    Use Twig in Template

---
+++ src/Loom/Template.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Loom;
+
+class Template
+{
+
+    // /**
+    //  * The singleton instance of this class
+    //  *
+    //  * @var    object $instance
+    //  * @access protected
+    //  */
+    // protected static $instance = null;
+
+    // public static function &getInstance()
+    // {
+    //     if (null === self::$instance) {
+    //         self::$instance = new Template();
+    //     }
+
+    //     return self::$instance;
+    // }
+
+    
+    public function __construct()
+    {
+        require_once "vendor/twig/twig/lib/Twig/Autoloader.php";
+        \Twig_Autoloader::register();
+        
+        $this->loader = new \Twig_Loader_Filesystem("/Users/eirikref/projects/contentmagic/src/templates");
+        $this->twig = new \Twig_Environment($this->loader, array(
+            // "cache" => "/Users/eirikref/projects/contentmagic/var/cache",
+        ));
+    }
+
+    public function render($tpl)
+    {
+        $html = $this->twig->render($tpl);
+        print $html;
+    }
+}
